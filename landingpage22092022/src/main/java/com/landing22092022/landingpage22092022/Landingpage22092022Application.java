4fc78a3fc8895d59fc460b31b4c6aeb3bd08b298
Juan Avila <juan.avila@aldeamo.com>
2022-09-26 19:59:42 -0500

    Carga del Proyecto

---
+++ landingpage22092022/src/main/java/com/landing22092022/landingpage22092022/Landingpage22092022Application.java
@@ -0,0 +1,13 @@
+package com.landing22092022.landingpage22092022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Landingpage22092022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Landingpage22092022Application.class, args);
+	}
+
+}
---
+++ landingpage22092022/src/test/java/com/landing22092022/landingpage22092022/Landingpage22092022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landing22092022.landingpage22092022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Landingpage22092022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
